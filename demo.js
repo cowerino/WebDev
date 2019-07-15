447dd71a207482ed1a2c625dd80fccc7a163946d
Cowerino <nieve.rob@gmail.com>
2019-07-14 20:23:34 -0600

    added finish gif and tries counter to hi-lo game

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 
 var randomNum = Math.round(Math.random()*99)+1;
 console.log("The answer is "+randomNum+", Brother.");
-
+var tries=1;
 function sendData(form){
     var guessNum = form.inputbox.value;
     if(guessNum>randomNum)
@@ -14,8 +14,21 @@ function sendData(form){
     }
     else if(guessNum==randomNum){
         document.getElementById("cat").src="numberGuessWin.png";
+        document.body.style.background="url(party.gif)";
+
+            if(tries==1)
+            console.log("You got the answer in "+tries+" try.")
+            else if(tries>1)
+            console.log("You got the answer in "+tries+" tries.")
     }
     else{
         document.getElementById("cat").src="numberGuessWhat.png";
     }
+    tries++;
 }
+
+
+//questions:
+//how to make background party gif non-repeat across the page
+//
+//why does page refresh when I press enter on the input-box?
