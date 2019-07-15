69c6d937d7794305bda68c0c092f15b80f3a0c56
Cowerino <nieve.rob@gmail.com>
2019-07-14 20:01:07 -0600

    Hi-Lo Game finished, set up js and fixed css

--- demo.js
+++ demo.js
@@ -1,5 +1,21 @@
-let name="Gupta";
-function sayYourName(yourName) {
-    console.log("Shalom Brother "+ yourName +", how do you do?");
+
+var randomNum = Math.round(Math.random()*99)+1;
+console.log("The answer is "+randomNum+", Brother.");
+
+function sendData(form){
+    var guessNum = form.inputbox.value;
+    if(guessNum>randomNum)
+    {
+        document.getElementById("cat").src="numberGuess2High.png";
+    }
+    else if(guessNum<randomNum)
+    {
+        document.getElementById("cat").src="numberGuess2Low.png";
+    }
+    else if(guessNum==randomNum){
+        document.getElementById("cat").src="numberGuessWin.png";
+    }
+    else{
+        document.getElementById("cat").src="numberGuessWhat.png";
+    }
 }
-sayYourName(name);
